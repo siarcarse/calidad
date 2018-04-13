ea7e7cb9258e86ce08a8baf4bd05ecadf27afb6f
Simplicio Carcamo <siarcarse@gmail.com>
2018-04-13 19:48:09 -0300

    Ejercicio viernes 13 abril 2018

---
+++ ejercicio.js
@@ -0,0 +1,48 @@
+lab.test("Probando GET /API/USERS STATUS 200 Y cantidad de datos 3", function(done) {
+    var options = {
+        method: "GET",
+        url: "/api/users"
+    };
+
+    server.inject(options, function(response) {
+        var result = response.result;
+        code.expect(response.statusCode).to.equal(200);
+        code.expect(result).to.have.length(3);
+        done();
+    });
+});
+lab.test("Probando POST /API/USERS Con Todos los Datos STATUS 200 Y RESPUESTA", function(done) {
+    var options = {
+        method: "POST",
+        url: "/api/users",
+        payload: {
+            name: 'testing',
+            lastname: 'Hapi',
+            birthdate: '2018-04-01'
+        }
+    };
+
+    server.inject(options, function(response) {
+        var result = response.result;
+        code.expect(response.statusCode).to.equal(200);
+        code.expect(result).to.contain(['id', 'nombre', 'apellido']);
+        done();
+    });
+});
+
+lab.test("Probando DELETE /API/USERS STATUS 200 Y RESPUESTA", function(done) {
+    var options = {
+        method: "DELETE",
+        url: "/api/users",
+        payload: {
+            name: 'testing'
+        }
+    };
+
+    server.inject(options, function(response) {
+        var result = response.result;
+        code.expect(response.statusCode).to.equal(200);
+        code.expect(result).to.include({ deteled: true });
+        done();
+    });
+});
