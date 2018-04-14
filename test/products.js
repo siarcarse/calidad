591a075cee39b0db3a58c33cc2a5cd4352859933
Simplicio Carcamo <siarcarse@gmail.com>
2018-04-14 18:10:18 -0300

    Complemento ejercicio

--- test/products.js
+++ test/products.js
@@ -31,4 +31,57 @@ lab.experiment("Products", function() {
         code.expect(Routes[3].config.validate).to.be.instanceof(Object);
         done();
     });
+    /* DESDE AQUÍ LO NUEVO */
+    lab.test("Probando POST /API/PRODUCTS Con Todos los Datos STATUS 200 Y RESPUESTA", function(done) {
+        var options = {
+            method: "POST",
+            url: "/api/products",
+            payload: {
+                name: 'NOTEBOOK_TEST',
+                code: '09809809890',
+                price: 100000,
+                active: true
+            }
+        };
+
+        server.inject(options, function(response) {
+            var result = response.result;
+            code.expect(response.statusCode).to.equal(200);
+            code.expect(result).to.be.instanceof(Object);
+            code.expect(result).to.contain(['pk']);
+            done();
+        });
+    });
+    lab.test("Probando GET /API/PRODUCTS STATUS 200", function(done) {
+        var options = {
+            method: "GET",
+            url: "/api/products"
+        };
+
+        server.inject(options, function(response) {
+            var result = response.result;
+            code.expect(response.statusCode).to.equal(200);
+            code.expect(result).to.be.instanceof(Array);
+            code.expect(result.length).to.be.least(1);
+            done();
+        });
+    });
+
+    lab.test("Probando DELETE /API/PRODUCTS STATUS 200 Y RESPUESTA", function(done) {
+        var options = {
+            method: "DELETE",
+            url: "/api/products",
+            payload: {
+                name: 'NOTEBOOK_TEST'
+            }
+        };
+
+        server.inject(options, function(response) {
+            var result = response.result;
+            code.expect(response.statusCode).to.equal(200);
+            code.expect(result).to.part.include({ deleted: true });
+            done();
+        });
+    });
+    /* HASTA AQUÍ LO NUEVO */
 });
